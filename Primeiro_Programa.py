ebb5decd71330ebf6576fae14f211d9da81d920b
RinaldoDuarte729 <duarte.rinaldo@gmail.com>
2023-03-19 21:08:46 -0300

    Aulas de Python

---
+++ Primeiro_Programa.py
@@ -0,0 +1,6 @@
+print('Olá Python')
+a=2 #variável a recebe o valor de 2
+b=4 #variável b recebe o valor de 4
+c=2.5  #variável c recebe o valor de 2.5
+print("A vale 2, * = Multiplicação, B vale 4, Resultado é",a*b) #mostra a multiplicação de 2 x 4
+
